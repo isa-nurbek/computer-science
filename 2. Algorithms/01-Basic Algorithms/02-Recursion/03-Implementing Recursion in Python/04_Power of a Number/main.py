7fa56ebf62a2050ee75860fa98c034cce19596e1
Isradin <isradin.110593@gmail.com>
2025-04-01 03:02:28 +0300

    Implement code in Python

--- 2. Algorithms/01-Basic Algorithms/02-Recursion/03-Implementing Recursion in Python/04_Power of a Number/main.py
+++ 2. Algorithms/01-Basic Algorithms/02-Recursion/03-Implementing Recursion in Python/04_Power of a Number/main.py
@@ -1,1 +1,15 @@
 # Implementation in Python:
+
+
+def power(base, exp):
+    # Base case: any number raised to power 0 is 1
+    if exp == 0:
+        return 1
+    # Recursive case: multiply base by power(base, exp-1)
+    # This effectively breaks down the problem into smaller subproblems
+    else:
+        return base * power(base, exp - 1)
+
+
+# Test Case:
+print(power(2, 3))  # Output: 8
