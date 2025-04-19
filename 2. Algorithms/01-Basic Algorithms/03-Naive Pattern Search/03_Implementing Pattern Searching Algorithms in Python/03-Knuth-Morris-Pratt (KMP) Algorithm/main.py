05ffa1e422f1d14ca6311b2da1af5abff19b818b
Isradin <isradin.110593@gmail.com>
2025-04-19 03:41:16 +0300

    Implement Knuth-Morris-Pratt Algorithm in Python

---
+++ 2. Algorithms/01-Basic Algorithms/03-Naive Pattern Search/03_Implementing Pattern Searching Algorithms in Python/03-Knuth-Morris-Pratt (KMP) Algorithm/main.py
@@ -0,0 +1,55 @@
+# Implementation in Python:
+
+
+def kmp_search(text, pattern):
+    def compute_lps(pattern):
+        m = len(pattern)
+        lps = [0] * m
+        length = 0
+        i = 1
+        while i < m:
+            if pattern[i] == pattern[length]:
+                length += 1
+                lps[i] = length
+                i += 1
+            else:
+                if length != 0:
+                    length = lps[length - 1]
+                else:
+                    lps[i] = 0
+                    i += 1
+        return lps
+
+    n = len(text)
+    m = len(pattern)
+    lps = compute_lps(pattern)
+    result = []
+    i = j = 0
+
+    while i < n:
+        if pattern[j] == text[i]:
+            i += 1
+            j += 1
+
+        if j == m:
+            result.append(i - j)
+            j = lps[j - 1]
+        elif i < n and pattern[j] != text[i]:
+            if j != 0:
+                j = lps[j - 1]
+            else:
+                i += 1
+
+    return result
+
+
+# Example
+
+print("KMP:", kmp_search("AABAACAADAABAABA", "AABA"))
+
+# Output:
+
+"""
+KMP: [0, 9, 12]
+
+"""
